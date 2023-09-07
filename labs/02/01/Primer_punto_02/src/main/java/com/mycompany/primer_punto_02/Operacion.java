74f927de3aec1c9413afdd5972a85af480ee4456
JuanDb666 <141367187+JuanDb666@users.noreply.github.com>
2023-09-07 14:41:17 -0500

    Add files via upload

--- labs/02/01/Primer_punto_02/src/main/java/com/mycompany/primer_punto_02/Operacion.java
+++ labs/02/01/Primer_punto_02/src/main/java/com/mycompany/primer_punto_02/Operacion.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.primer_punto_02;
 
-/**
- *
- * @author Estudiante
- */
 public class Operacion {
-    
+    public static double[][] suma(int a, double [][]matrizA, double [][] matrizB, double [][] matrizC) {
+           for(int x=0; x<a; x++){
+               for(int y=0;y<a;y++){
+                   matrizC[x][y]= matrizA[x][y]+matrizB[x][y];
+               }
+           }
+            return matrizC;
+          }
 }
