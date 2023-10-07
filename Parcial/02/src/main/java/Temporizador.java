01ebaeaedd429bd7ccf13a2d2f0dd5bd633d9244
JuanDb666 <141367187+JuanDb666@users.noreply.github.com>
2023-10-06 19:32:21 -0500

    Add files via upload

--- Parcial/02/src/main/java/Temporizador.java
+++ Parcial/02/src/main/java/Temporizador.java
@@ -7,4 +7,42 @@ public class Temporizador extends TimerTask {
     private String mensaje;
     private static int contadorAlarmas = 1;
     private long tiempoProgramado;
+    
+    public Temporizador(int opcion, String mensaje, Long tiempoProgramado) {
+        this.opcion = opcion;
+        this.mensaje = mensaje;
+        this.tiempoProgramado = tiempoProgramado;
+    }
+    public Temporizador(int opcion, Long tiempoProgramado) {
+        this.opcion = opcion;
+        this.tiempoProgramado = tiempoProgramado;
+    }
+    
+    public int getOpcion() {
+        return opcion;
+    }
+    public String getMensaje() {
+        return mensaje;
+    }
+    public long getTiempoProgramado() {
+        return tiempoProgramado;
+    }
+    @Override
+    public void run() {
+        switch (opcion) {
+            case 1:
+                System.out.println("*****Mensaje programado: " + mensaje+"*****");
+                break;
+            case 2:
+                System.out.println("*****Alarma " + contadorAlarmas+"*****");
+                contadorAlarmas++;
+                break;
+            case 3:
+                System.out.println("*****Saludo: Hola :)*****");
+                break;
+            case 4:
+                System.out.println("*****Despedida: Chao :(*****");
+                break;
+        }
+    }
 }
