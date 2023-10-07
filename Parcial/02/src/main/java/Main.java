e8cf87485f5c7cf825fa2846702ffc82aad0a5b0
JuanDb666 <141367187+JuanDb666@users.noreply.github.com>
2023-10-06 20:35:40 -0500

    Add files via upload

--- Parcial/02/src/main/java/Main.java
+++ Parcial/02/src/main/java/Main.java
@@ -25,7 +25,81 @@ public class Main {
             System.out.println("7. Salir");
 
             int opcion = scanner.nextInt();
+
+            if (opcion == 7) {
+                detenerTodosLosTemporizadores();
+                break;
+            }
+
+            if (opcion == 5) {
+                System.out.println("Ingrese el número de temporizador que desea detener:");
+                int numeroTemporizador = scanner.nextInt();
+                detenerTemporizador(numeroTemporizador);
+            } else if (opcion == 6) {
+                System.out.println("Ingrese el número de temporizador que desea reiniciar:");
+                int numeroTemporizador = scanner.nextInt();
+                reiniciarTemporizador(numeroTemporizador);
+            } else {
+                long tiempoEspera;
+                tiempoEspera = obtenerTiempoEspera(scanner);
             
+                if (opcion == 1) {
+                    System.out.println("Ingrese el mensaje para el temporizador:");
+                    scanner.nextLine();
+                    String mensaje = scanner.nextLine();
+                    Temporizador tarea = new Temporizador(opcion, mensaje, tiempoEspera);
+                    int contTemporizador = temporizadoresActivos.size()+1;
+                    System.out.println("Se programo el temporizador "+contTemporizador);
+                    temporizadoresActivos.add(tarea);
+                    timer.schedule(tarea, tiempoEspera);
+                } else {
+                    Temporizador tarea = new Temporizador(opcion, tiempoEspera);
+                    int contTemporizador = temporizadoresActivos.size()+1;
+                    System.out.println("Se programo el temporizador "+contTemporizador);
+                    temporizadoresActivos.add(tarea);
+                    timer.schedule(tarea, tiempoEspera);
+                }
+            }   
         }
+    }
+
+    private static void detenerTemporizador(int numeroTemporizador) {
+        if (numeroTemporizador >= 1 && numeroTemporizador <= temporizadoresActivos.size()) {
+            Temporizador tarea = temporizadoresActivos.get(numeroTemporizador - 1);
+            tarea.cancel();
+            temporizadoresActivos.remove(tarea);
+            System.out.println("Temporizador " + numeroTemporizador + " detenido.");
+        } else {
+            System.out.println("Temporizador no válido.");
+        }
+    }
+
+    private static void reiniciarTemporizador(int numeroTemporizador) {
+        if (numeroTemporizador >= 1 && numeroTemporizador <= temporizadoresActivos.size()) {
+            
+            Temporizador tarea = temporizadoresActivos.get(numeroTemporizador - 1);
+
+            int opcion = tarea.getOpcion();
+            String mensaje = tarea.getMensaje();
+            long tiempoProgramado = tarea.getTiempoProgramado();
+            System.out.println(tiempoProgramado);
+
+            tarea.cancel();
+
+            timer.schedule(new Temporizador(opcion, mensaje, tiempoProgramado), tiempoProgramado);
+            System.out.println("Temporizador " + numeroTemporizador + " reiniciado.");
+        } else {
+            System.out.println("Temporizador no válido.");
+        }
+    }
+
+    private static void detenerTodosLosTemporizadores() {
+        timer.cancel();
+        System.out.println("Todos los temporizadores detenidos.");
+    }
+    
+    private static long obtenerTiempoEspera(Scanner scanner) {
+        System.out.println("Ingrese el tiempo de espera en segundos:");
+        return (scanner.nextLong() * 1000);
     }
 }
