c2e98a709daaa77e2a80f6eb8b3a59507c298f34
JuanDb666 <141367187+JuanDb666@users.noreply.github.com>
2023-10-06 20:03:39 -0500

    Add files via upload

--- Parcial/02/src/main/java/Main.java
+++ Parcial/02/src/main/java/Main.java
@@ -8,4 +8,24 @@ import java.util.Timer;
 public class Main {
     private static Timer timer = new Timer();
     private static List<Temporizador> temporizadoresActivos = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("-----------------------------------------");
+            System.out.println("\t\tProgramador de tareas");
+            System.out.println("Seleccione una tarea a programar:");
+            System.out.println("1. Programar Mensaje");
+            System.out.println("2. Programar Alarma");
+            System.out.println("3. Programar Saludo");
+            System.out.println("4. Programar Despedida");
+            System.out.println("5. Detener temporizador especifico");
+            System.out.println("6. Reiniciar temporizador especifico");
+            System.out.println("7. Salir");
+
+            int opcion = scanner.nextInt();
+            
+        }
+    }
 }
