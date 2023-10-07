c656ad02f5f99ddc7173a2ac3d3206508ac21fac
JuanDb666 <141367187+JuanDb666@users.noreply.github.com>
2023-10-06 19:01:06 -0500

    Add files via upload

---
+++ Parcial/02/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Timer;
+
+//Autores: Cristhian David Rosales y Juan David Blanco 
+
+public class Main {
+    private static Timer timer = new Timer();
+    private static List<Temporizador> temporizadoresActivos = new ArrayList<>();
+}
---
+++ Parcial/02/src/main/java/Temporizador.java
@@ -0,0 +1,10 @@
+import java.util.TimerTask;
+
+//Autores: Cristhian David Rosales y Juan David Blanco 
+
+public class Temporizador extends TimerTask {
+    private int opcion;
+    private String mensaje;
+    private static int contadorAlarmas = 1;
+    private long tiempoProgramado;
+}
